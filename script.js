9bb747feffa8af72cce626c8201d3d367f564e2c
Drillbur123 <dylannguyen0826@gmail.com>
2025-03-06 09:58:54 -0500

    peepee poopoo

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    let c = document.getElementById("testCanvas");
+    let ctx = c.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.fillRect(125,25,150,50);
+});
